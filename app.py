ad24b7a2218a0b9bf7a895b4b8fe919e49eff0da
joeydeluca <joeydeluca@hotmail.com>
2016-10-23 16:39:21 -0400

    image compare service

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, Response, request
+from flask import Flask, Response, request, jsonify
 from scipy.spatial import distance as dist
+import sys
 import numpy as np
 import urllib
 import cv2
@@ -8,39 +9,40 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+
+  # Get input data
   url1 = request.args.get('url1')
   url2 = request.args.get('url2')
 
+  # Validate input data
   if not url1 or not url2:
-    return Response(response='Parameters must include url1 and url2',
-                    status=400, \
-                    mimetype="application/json")
+    resp = jsonify({
+      'status'  : 'error',
+      'errorMessage': 'Parameters must include url1 and url2'
+    })
+    resp.status_code = 400
+    return resp;
 
+  # Compare images. The lower the number, the closer the match
+  try:
+    compareResult = dist.euclidean(get_histogram_from_url(url1), get_histogram_from_url(url2))
+  except:
+    errorMessage = sys.exc_info()[0]
+    print("Unexpected error:", errorMessage)
+    resp = jsonify({
+      'status'  : 'error',
+      'errorMessage': errorMessage
+    })
+    resp.status_code = 500
+    return resp;
 
-  image1 = url_to_image(url1)
-  image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+  resp = jsonify({
+    'status'  : 'success',
+    'result' : str(compareResult),
+  })
+  resp.status_code = 200
+  return resp;
 
-  hist1 = cv2.calcHist([image1], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-  hist1 = cv2.normalize(hist1).flatten()
-
-  image2 = url_to_image(url2)
-  image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
-
-  hist2 = cv2.calcHist([image2], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
-  hist2 = cv2.normalize(hist2).flatten()
-
-  SCIPY_METHODS = (
-    ("Euclidean", dist.euclidean),
-    ("Manhattan", dist.cityblock),
-    ("Chebysev", dist.chebyshev))
-
-
-  d = dist.euclidean(hist1, hist2)
-
-
-  return Response(response=str(d),
-                  status=200, \
-                  mimetype="application/json")
 
 def url_to_image(url):
   print "downloading %s" % (url)
@@ -52,6 +54,15 @@ def url_to_image(url):
 
   # return the image
   return image
+
+def get_histogram_from_url(url):
+  image = url_to_image(url)
+  image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+  hist = cv2.calcHist([image], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+  hist = cv2.normalize(hist).flatten()
+
+  return hist;
 
 if __name__ == '__main__':
     app.run()
