f732f1dde4bb931b88a012b553153a11bdfc881f
joeydeluca <joeydeluca@hotmail.com>
2016-10-23 15:51:31 -0400

    testing

--- app.py
+++ app.py
@@ -9,14 +9,33 @@ def hello_world():
   url1 = request.args.get('url1')
   url2 = request.args.get('url2')
 
-  image = cv2.imread(url1)
-
   if not url1 or not url2:
     return Response(response='Parameters must include url1 and url2',
-      status=400, \
-      mimetype="application/json")
+                    status=400, \
+                    mimetype="application/json")
 
-  return Response(response='cool',
+  image1 = cv2.imread(url1)
+  image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
+
+  hist1 = cv2.calcHist([image1], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+  hist1 = cv2.normalize(hist1).flatten()
+
+  image2 = cv2.imread(url2)
+  image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
+
+  hist2 = cv2.calcHist([image2], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
+  hist2 = cv2.normalize(hist1).flatten()
+
+  SCIPY_METHODS = (
+    ("Euclidean", dist.euclidean),
+    ("Manhattan", dist.cityblock),
+    ("Chebysev", dist.chebyshev))
+
+
+  d = dist.euclidean(hist1, hist2)
+
+
+  return Response(response=d,
                   status=200, \
                   mimetype="application/json")
 
