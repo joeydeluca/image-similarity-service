0928595d7d2a9a5424d2d6d98fb9b145719905bf
joeydeluca <joeydeluca@hotmail.com>
2016-10-23 16:00:05 -0400

    testing

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, Response, request
 from scipy.spatial import distance as dist
+import numpy as np
+import urllib
 import cv2
 
 app = Flask(__name__)
@@ -14,13 +16,14 @@ def hello_world():
                     status=400, \
                     mimetype="application/json")
 
-  image1 = cv2.imread(url1)
+
+  image1 = cv2.imread(url_to_image(url1))
   image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2RGB)
 
   hist1 = cv2.calcHist([image1], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
   hist1 = cv2.normalize(hist1).flatten()
 
-  image2 = cv2.imread(url2)
+  image2 = cv2.imread(url_to_image(url2))
   image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2RGB)
 
   hist2 = cv2.calcHist([image2], [0, 1, 2], None, [8, 8, 8], [0, 256, 0, 256, 0, 256])
@@ -38,6 +41,17 @@ def hello_world():
   return Response(response=d,
                   status=200, \
                   mimetype="application/json")
+
+def url_to_image(url):
+  print "downloading %s" % (url)
+  # download the image, convert it to a NumPy array, and then read
+  # it into OpenCV format
+  resp = urllib.urlopen(url)
+  image = np.asarray(bytearray(resp.read()), dtype="uint8")
+  image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+
+  # return the image
+  return image
 
 if __name__ == '__main__':
     app.run()
